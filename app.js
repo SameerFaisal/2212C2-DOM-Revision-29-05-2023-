33ef61b7267f74637cfcf0b743eeccaf095c89f3
SameerFaisal <sameerfaisal46@gmail.com>
2023-05-29 13:19:36 +0500

    2212C2 DOM Revision (29-05-2023)

---
+++ app.js
@@ -0,0 +1,66 @@
+// function display(){
+//     // let fn=document.getElementById("fname")
+// let fn=document.querySelector("#fname").value
+// let ln=document.querySelector("#lname").value
+// let result=fn+" "+ln
+// document.getElementById("output").innerHTML=result
+// }
+
+// function calc(){
+//     let x=document.getElementById("num1").value
+//     let y=document.getElementById("num2").value
+//     let op=document.getElementById("operator").value
+//     switch(op){
+//         case "+":
+//             document.getElementById("output").innerHTML=parseFloat(x)+parseFloat(y)
+//             break
+//         case "-":
+//             document.getElementById("output").innerHTML=x-y
+//             break
+//         case "*":
+//             document.getElementById("output").innerHTML=x*y
+//             break
+//         case "/":
+//             document.getElementById("output").innerHTML=x/y
+//             break
+//         default:
+//             document.getElementById("output").innerHTML="Invalid Operation"
+//             break
+
+//     }
+// }
+
+
+// function add(){
+//     let x=document.getElementById("num1").value
+//     let y=document.getElementById("num2").value
+//     let result=Number(x)+Number(y)
+//     document.getElementById("output").innerHTML=result
+// }
+// function sub(){
+//     let x=document.getElementById("num1").value
+//     let y=document.getElementById("num2").value
+//     let result=Number(x)-Number(y)
+//     document.getElementById("output").innerHTML=result
+// }
+// function mul(){
+//     let x=document.getElementById("num1").value
+//     let y=document.getElementById("num2").value
+//     let result=Number(x)*Number(y)
+//     document.getElementById("output").innerHTML=result
+// }
+// function div(){
+//     let x=document.getElementById("num1").value
+//     let y=document.getElementById("num2").value
+//     let result=Number(x)/Number(y)
+//     document.getElementById("output").innerHTML=result.toFixed(4)
+// }
+
+// function change(){
+//     document.getElementById("i1").src="https://i.gifer.com/origin/b1/b103363ac155bfa8895c2bf3cf0b4b41.gif"
+  
+// }
+
+// const revert=()=>{
+//     document.getElementById("i1").src="https://thumbs.gfycat.com/FlatGrouchyGodwit-size_restricted.gif"
+// }
